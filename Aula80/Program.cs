a9065f9c39780aac409af29e6b11deb9f3aca8cf
GRUPPY02 <GRUPPY02@DESKTOP-B3K8HS6>
2025-03-24 17:59:53 -0300

    Atividade

--- Aula80/Program.cs
+++ Aula80/Program.cs
@@ -4,14 +4,13 @@ using System.Collections.Generic;
 
 
 
-List<int> diagonalPrincipal = new List<int>();
+//List<int> diagonalPrincipal = new List<int>();
 
-List<int> numerosNegativos = new List<int>();
 
 Console.Write("Digite a ordem da matriz:");
 int ordemMatriz = int.Parse(Console.ReadLine());
 
-
+int count = 0;
 int[,] matriz = new int[ordemMatriz,ordemMatriz];
 
 
@@ -24,20 +23,45 @@ for (int i = 0; i < ordemMatriz; i++)
     for( int j = 0; j < ordemMatriz; j++)
     {
         matriz[i,j] = int.Parse(numeros[j]);
-        if (i == j)
-            diagonalPrincipal.Add(int.Parse(numeros[j]));
-        if (j < 0)
-            numerosNegativos.Add(int.Parse(numeros[j]));
+        //if (i == j)
+        //    diagonalPrincipal.Add(int.Parse(numeros[j]));
     }
 
 }
-Console.WriteLine("Diagonal Principal: ");
 
-foreach (int item in diagonalPrincipal) {
-    Console.WriteLine( item + " ");
-}
-Console.Write("Negative numbers = ");
-foreach (int negative in numerosNegativos)
+Console.WriteLine("Main diagonal: ");
+for (int i = 0; i  < ordemMatriz; i++)
 {
-    Console.WriteLine( negative + " ");
+    Console.Write(matriz[i,i] + " ");
 }
+Console.WriteLine();
+
+
+Console.WriteLine("Secundary diagonal: ");
+for (int i = 0; i < ordemMatriz; i++)
+{
+    Console.Write(matriz[i, ordemMatriz - 1 -i] + " ");
+}
+
+//foreach (int item in diagonalPrincipal) {
+//    Console.Write( item + " ");
+//}
+
+Console.WriteLine();
+
+
+for  (int i = 0;i < ordemMatriz; i++)
+{
+    for (int j = 0; j < ordemMatriz; j++)
+    {
+        if (matriz[i, j] < 0 )
+        {
+           count++;
+
+        }
+            
+    }
+}
+Console.WriteLine("Negative numbers = " + count);
+
+Console.ReadLine();
---
+++ Aula81/Program.cs
@@ -0,0 +1,55 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Console.Write("Digite o número de linhas e colunas da matriz: ");
+        string[] line = Console.ReadLine().Split(' ');
+        
+        int m = int.Parse(line[0]);
+        int n = int.Parse(line[1]);
+
+        int[,] matriz = new int[m, n];
+
+        Console.WriteLine("Digite os números da coluna:");
+        for (int i = 0; i < m; i++)
+        {
+            string[] numeros = Console.ReadLine().Split(' ');
+            for (int j = 0; j < n; j++)
+            {
+                matriz[i, j] = int.Parse(numeros[j]);
+            }
+        }
+
+        Console.Write("Digite um número que contem na matriz:");
+        int x = int.Parse(Console.ReadLine());
+
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (matriz[i, j] == x)
+                {
+                    Console.WriteLine($"Posição = {i + 1} , {j + 1} : ");
+                    if (j > 0)
+                    {
+                        Console.WriteLine("Número a esquerda: " + matriz[i, j - 1]);
+                    }
+                    if (i > 0)
+                    {
+                        Console.WriteLine("Número a acima: " + matriz[i - 1, j]);
+                    }
+                    if (j < n - 1)
+                    {
+                        Console.WriteLine("Número a direita: " + matriz[i, j + 1]);
+                    }
+                    if (i < n - 1)
+                    {
+                        Console.WriteLine("Número a abaixo: " + matriz[i + 1, j]);
+                    }
+                }
+            }
+        }
+    }
+}
