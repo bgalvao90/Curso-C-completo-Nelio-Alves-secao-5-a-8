8d8d1e00e18d2d377517e3d950d17894bf233a33
GRUPPY02 <GRUPPY02@DESKTOP-B3K8HS6>
2025-03-24 12:01:54 -0300

    asasa

---
+++ Aula80/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows.Markup;
+using System.Globalization;
+using System.Collections.Generic;
+
+
+
+List<int> diagonalPrincipal = new List<int>();
+
+List<int> numerosNegativos = new List<int>();
+
+Console.Write("Digite a ordem da matriz:");
+int ordemMatriz = int.Parse(Console.ReadLine());
+
+
+int[,] matriz = new int[ordemMatriz,ordemMatriz];
+
+
+for (int i = 0; i < ordemMatriz; i++)
+{
+
+    string[] numeros = Console.ReadLine().Split(' ');
+
+
+    for( int j = 0; j < ordemMatriz; j++)
+    {
+        matriz[i,j] = int.Parse(numeros[j]);
+        if (i == j)
+            diagonalPrincipal.Add(int.Parse(numeros[j]));
+        if (j < 0)
+            numerosNegativos.Add(int.Parse(numeros[j]));
+    }
+
+}
+Console.WriteLine("Diagonal Principal: ");
+
+foreach (int item in diagonalPrincipal) {
+    Console.WriteLine( item + " ");
+}
+Console.Write("Negative numbers = ");
+foreach (int negative in numerosNegativos)
+{
+    Console.WriteLine( negative + " ");
+}
