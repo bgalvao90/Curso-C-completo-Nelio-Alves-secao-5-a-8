1a5c092ab300d61e3d3cb3fe8c4d9d9822d797f2
GRUPPY02 <GRUPPY02@DESKTOP-B3K8HS6>
2025-03-25 18:09:10 -0300

    aTIVIDADE

---
+++ Aula104/Entities/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula104.Entities
+{
+    class Comment
+    {
+        public Comment()
+        {
+        }
+
+        public string Text { get; set; }
+
+        public Comment(string text)
+        {
+            this.Text = text;
+        }
+    }
+}
