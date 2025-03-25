e95e6b64a93f3d54da7d1878f9fa068f57616ffd
usuario <usuario@DESKTOP-RSA23LP>
2025-03-24 23:20:27 -0300

    Atividades

---
+++ aula97/Entities/enums/OrderStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula97.Entities.enums
+{
+    enum OrderStatus: int
+    {
+        PendingPayment, 
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
---
+++ aula97/Program.cs
@@ -0,0 +1,13 @@
+﻿using aula97.Entities;
+using aula97.Entities.enums;
+
+Order order = new Order() { Id = 109, Moment = DateTime.Now , Status = OrderStatus.PendingPayment };
+
+Console.WriteLine(order);
+
+string txt = OrderStatus.PendingPayment.ToString();
+
+OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+Console.WriteLine(txt);
+Console.WriteLine(os);
